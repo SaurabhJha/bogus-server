f4df629562aa4fd189c5a8a15051ba241b5def49
Saurabh Jha <saurabh.jhaa@gmail.com>
2016-04-24 16:48:59 +0530

    Add handler for interrupt

--- main.c
+++ main.c
@@ -1,28 +1,39 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netdb.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
+#include "server.h"
 
-#define BACKLOG 5
+#define BACKLOG 1024
 #define PORT "3490"
 
+void interrupt_signal_handler(int signum)
+{
+  fprintf(stdout, "Caught signal %d: %s\n", signum, strsignal(signum));
+  fprintf(stdout, "Closing socket %d\n", server_socket);
+  if (close(server_socket) < 0) {
+    perror("Failed to close server_socke (ignoring)\n");
+  }
+  exit(0);
+}
 
 int main(int argc, char *argv[])
 {
   struct addrinfo hints, *res, *p;
   struct sockaddr *client_address;
-  struct sockaddr_in *client_address_in;
   socklen_t *client_address_len;
-  int status, server_socket, client_socket, yes = 1;
-  char *message, client_ip_string[INET6_ADDRSTRLEN];
+  int status, client_socket, yes = 1;
+  char *message;
+
+  signal(SIGINT, interrupt_signal_handler);
 
   memset(&hints, 0, sizeof(hints));
-
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
@@ -32,7 +43,6 @@ int main(int argc, char *argv[])
     return 2;
   }
 
-  server_socket = 0;
   for (p = res; p != NULL; p = p->ai_next) {
     server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
     if (server_socket == -1) {
@@ -64,6 +74,11 @@ int main(int argc, char *argv[])
 
   while (1) {
     client_socket = accept(server_socket, client_address, client_address_len);
+    if (client_socket < 0) {
+      fprintf(stderr, "Not able to accept connection.\n");
+    } else {
+      fprintf(stdout, "Got a new connection.\n");
+    }
     message = "Howdy?\n"; // What if this message is dynamic?
     send(client_socket, message, strlen(message), 0);
     close(client_socket);
